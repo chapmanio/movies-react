7b7e8ca2b0f3429da63f086d8c9c7dfe1b50d046
Richard Chapman <rcchapman85@gmail.com>
2022-01-20 15:02:02 +0000

    Testing same-origin credentials

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -41,7 +41,7 @@ export const apiFetch = async <T>(url: string, init?: RequestInit | undefined): 
   // Call API
   const response = await fetch(mergedUrl, {
     ...init,
-    credentials: 'include',
+    credentials: 'same-origin',
   });
 
   if (!response.ok) {
