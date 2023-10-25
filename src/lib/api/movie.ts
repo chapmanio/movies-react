6880861802764ede56d0cf161244af283c26b916
Richard Chapman <rcchapman85@gmail.com>
2023-10-25 16:37:03 +0100

    Remove redundant api calls

--- src/lib/api/movie.ts
+++ src/lib/api/movie.ts
@@ -1,24 +1,12 @@
 import type {
   CreditsResponse,
   MovieResponse,
-  MovieResultsResponse,
-} from 'moviedb-promise/dist/request-types';
+} from "moviedb-promise/dist/request-types";
 
-import { apiFetch } from '../api';
-
-// Types
-type SearchArgs = {
-  query: string;
-  page: number;
-};
+import { apiFetch } from "../api";
 
 type GetArgs = {
   id: number;
-};
-
-// Exports
-export const searchMovie = async ({ query, page }: SearchArgs) => {
-  return apiFetch<MovieResultsResponse>(`/movie/search?query=${query}&page=${page}`);
 };
 
 export const getMovie = async ({ id }: GetArgs) => {
