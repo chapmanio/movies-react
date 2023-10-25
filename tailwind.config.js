604876857a48eaafab7f9c3176e3a0ee6c0073ab
Richard Chapman <rcchapman85@gmail.com>
2023-10-25 11:25:50 +0100

    Switch to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
